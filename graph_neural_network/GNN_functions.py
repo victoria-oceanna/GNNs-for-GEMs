71ad93aabee27a72dde8aa0b52977e7f1e786fb2
victoria-oceanna <75588030+victoria-oceanna@users.noreply.github.com>
2023-11-15 00:56:23 -0600

    Update GNN_functions.py

--- graph_neural_network/GNN_functions.py
+++ graph_neural_network/GNN_functions.py
@@ -1,6 +1,15 @@
 import torch
 
 def train(model, train_loader, device, criterion, optimizer):
+     """Trains GNN model
+
+    :param model: GNN model to train
+    :param train_loader: Dataloader for training portion of dataset
+    :param device: CUDA device for GPU computing
+    :param criterion: Training criterion
+    :param optimizer: Optimization algorithm
+    :return: None
+    """
     model.train()
 
     for data in train_loader: 
@@ -13,6 +22,13 @@ def train(model, train_loader, device, criterion, optimizer):
         optimizer.zero_grad() 
 
 def test(model, test_loader, device):
+    """Applies GNN model to test dataset and evaluates performance
+
+    :param model: Trained GNN model to test
+    :param train_loader: Dataloader for test portion of dataset
+    :param device: CUDA device for GPU computing
+    :return: Model accuracy (0-1)
+    """
     model.eval()
     correct = 0
     for data in test_loader:
